abe6c2795aaed6900e1c5d86f0aa904f2a299e84
Ivar Refsdal <refsdal.ivar@gmail.com>
2022-06-13 10:46:37 +0200

    Prepare release

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 git update-index --refresh
 git diff-index --quiet HEAD --
@@ -9,8 +9,8 @@ VERSION="v0.1.$(git rev-list --count HEAD)"
 
 echo "Releasing $VERSION"
 
-docker build --tag ivarref/mikkmokk-proxy:"$VERSION" .
-docker push ivarref/mikkmokk-proxy:"$VERSION"
+docker build --tag docker.io/ivarref/mikkmokk-proxy:"$VERSION" .
+docker push docker.io/ivarref/mikkmokk-proxy:"$VERSION"
 
 git tag -a "$VERSION" -m "Release $VERSION"
 git push --follow-tags
